ce6b34114533ce4ec7b89b608dc59c7721031be6
erajkhatiwada <eraj.khatiwada@selu.edu>
2020-01-26 18:30:54 -0600

    backend setup

---
+++ TutorHelpBackend/TutorHelpBackend/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TutorHelpBackend.Models;
+
+namespace TutorHelpBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly TutorHelpContext _context;
+
+        public UserController(TutorHelpContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public  IActionResult GetResult()
+        {
+            var result = _context.User.ToList();
+            return Ok(result);
+        }
+    }
+}
---
+++ TutorHelpBackend/TutorHelpBackend/Models/TutorHelpContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TutorHelpBackend.Models
+{
+    public partial class TutorHelpContext : DbContext
+    {
+        public TutorHelpContext()
+        {
+        }
+
+        public TutorHelpContext(DbContextOptions<TutorHelpContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-ATE4GIM\\SQLEXPRESS;Database=TutorHelp;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Email).IsRequired();
+
+                entity.Property(e => e.FirstName).IsRequired();
+
+                entity.Property(e => e.LastName).IsRequired();
+
+                entity.Property(e => e.Password).IsRequired();
+
+                entity.Property(e => e.Username).IsRequired();
+            });
+        }
+    }
+}
